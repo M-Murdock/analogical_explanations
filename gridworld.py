c925c9a1d95ff6fee558e574d7501d7ec0a61584
Mavis Murdock <mavis.murdock@tufts.edu>
2024-10-10 12:50:00 -0400

    Find trajectories which are closest in reward

--- gridworld.py
+++ gridworld.py
@@ -9,25 +9,41 @@ from simple_rl.tasks.grid_world import GridWorldMDPClass
 from simple_rl.agents import QLearningAgent
 from mdp import run_mdp
 from simple_rl.run_experiments import run_single_agent_on_mdp
-from trajectory import Traj, create_embeddings
+from trajectory import Traj, create_embeddings, find_closest_reward
 
 def main():
-    # make_grid_world_from_file(file_name, randomize=False, num_goals=1, name=None, goal_num=None, slip_prob=0.0):
+    # make_grid_world_from_file("easygrid.txt", randomize=True, num_goals=1, name=None, goal_num=None, slip_prob=0.0)
     mdp = GridWorldMDPClass.make_grid_world_from_file("easygrid.txt")
     ql_agent = QLearningAgent(actions=mdp.get_actions()) 
-    
-    # print(mdp.get_goal_locs())
-    
     # train policy
     run_single_agent_on_mdp(ql_agent, mdp, episodes=1000, steps=200, verbose=False)
-    
     # run agent according to policy
     traj, reward = run_mdp(ql_agent, mdp)
-    # print(traj)
-
-    traj_embedding_info = [(reward, traj, mdp)]
-    # print(trajectory.embedding)
-    create_embeddings(traj_embedding_info)
+    
+    # -----------------------
+    # create second trajectory
+    mdp2 = GridWorldMDPClass.make_grid_world_from_file("easygrid.txt", randomize=True, num_goals=1, name=None, goal_num=None, slip_prob=0.1)
+    ql_agent2 = QLearningAgent(actions=mdp.get_actions()) 
+    # train policy
+    run_single_agent_on_mdp(ql_agent2, mdp2, episodes=1000, steps=200, verbose=False)
+    # run agent according to policy
+    traj2, reward2 = run_mdp(ql_agent2, mdp2)
+    # -------------------------
+    # -----------------------
+    # create third trajectory
+    mdp3 = GridWorldMDPClass.make_grid_world_from_file("easygrid.txt", randomize=True, num_goals=1, name=None, goal_num=None, slip_prob=0.5)
+    ql_agent3 = QLearningAgent(actions=mdp.get_actions()) 
+    # train policy
+    run_single_agent_on_mdp(ql_agent3, mdp3, episodes=1000, steps=200, verbose=False)
+    # run agent according to policy
+    traj3, reward3 = run_mdp(ql_agent3, mdp3)
+    # -------------------------
+    
+    # traj_embedding_info = [(traj, reward, mdp)]
+    traj_embedding_info = [(traj, reward, mdp), (traj2, reward2, mdp2), (traj3, reward3, mdp3)]
+    trajectories = create_embeddings(traj_embedding_info)
+    find_closest_reward(trajectories, reference_traj=trajectories[0], K=1)
+    
     # visualize policy
     mdp.visualize_agent(ql_agent)
     
--- trajectory.py
+++ trajectory.py
@@ -21,24 +21,27 @@ def create_embeddings(traj_info_list):
 
     # input = state-action sequence  
     # output = environment??
-
+ 
 
     # get a list of state/actions in trajectory
     # [[traj1],[traj2], ...]
     input_list = []
     for t in traj_info_list:
-        input_list.append(traj_to_int(t[1]))
+        input_list.append(traj_to_int(t[0]))
+        
+    # make all trajectories of the same length
+    input_list = _make_length_uniform(input_list)
 
-    print("INPUT: ", input_list)
+    # print("INPUT: ", input_list)
 
     output_list = [8 for i in input_list]
-    print("OUTPUT: ", output_list)
+    # print("OUTPUT: ", output_list)
 
     model = AE(input_dim=len(input_list))
- 
+    
+    # print("FEATURES: ", input_list)
     features = torch.FloatTensor(input_list)
     labels = torch.FloatTensor(output_list)
- 
 
     train = data_utils.TensorDataset(features, labels)
     train_loader = data_utils.DataLoader(train, batch_size=50, shuffle=True)
@@ -48,17 +51,33 @@ def create_embeddings(traj_info_list):
     
     trajectories = []
     for traj_num in range(0, len(traj_info_list)):
-        # trajectories.append(Traj(traj_info_list[traj_num][1], traj_info_list[traj_num][0], model.encoder[0].weight[traj_num]))
-        trajectories.append(Traj(traj_info_list[traj_num][0], traj_info_list[traj_num][1], model.encoder[traj_num].weight))
+        # trajectories.append(Traj(traj_info_list[traj_num][0], traj_info_list[traj_num][1], model.encoder[traj_num].weight))
+        trajectories.append(Traj(traj_info_list[traj_num][0], traj_info_list[traj_num][1], model.encoder[traj_num]))
         
 
     return trajectories
+
+def find_closest_reward(trajectories, reference_traj, K=5):
+    # create a list of trajectories that is sorted by reward
+    sorted_trajectories = sorted(trajectories, key=lambda x: x.reward)
+    sorted_trajectories.remove(reference_traj)
+
+    # find the absolute difference in reward between reference_traj and each trajectory
+    absolute_difference = [abs(x.reward - reference_traj.reward) for x in sorted_trajectories]
+    print(absolute_difference)
+    
+    # find K number of closest trajectories 
+    closest_indices = sorted(range(len(absolute_difference)), key=lambda sub: absolute_difference[sub])[:K]
+    closest_trajectories = [sorted_trajectories[i] for i in closest_indices]
+    
+    return closest_trajectories
+
     
 def traj_to_int(traj):
     numerical_traj = []
     pair_sum = 0
     
-    # (state, action), (state, action)]
+    # (state, action), (state, action)
     for state_action in traj:
         pair = str(state_action[0]) + str(state_action[1])
         for letter in pair:
@@ -66,3 +85,18 @@ def traj_to_int(traj):
 
         numerical_traj.append(pair_sum)
     return numerical_traj
+
+# NOTE: this definitely isn't the best way to do this
+def _make_length_uniform(input_list):
+    longest = 0
+    # find longest trajectory
+    for i in input_list:
+        if len(i) > longest:
+            longest = len(i)
+            
+    # make the other trajectories the same length
+    for i in input_list:
+        if len(i) < longest:
+            i.extend([0]*(longest - len(i)))
+    
+    return input_list 
