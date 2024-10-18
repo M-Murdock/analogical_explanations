d52aa4a07c59a50b3ecd9980bbedcba5d023d8f8
Mavis Murdock <mavis.murdock@tufts.edu>
2024-10-18 10:17:32 -0400

    clean up, allow for more embedding types

--- gridworld.py
+++ gridworld.py
@@ -7,47 +7,106 @@ from __future__ import print_function
 from simple_rl.tasks import GridWorldMDP
 from simple_rl.tasks.grid_world import GridWorldMDPClass
 from simple_rl.agents import QLearningAgent
-from mdp import run_mdp
+# from mdp import run_mdp, generate_policies_for_agents
 from simple_rl.run_experiments import run_single_agent_on_mdp
-from trajectory import Traj, create_embeddings, find_closest_reward
+from trajectory import create_embeddings
+# , find_closest_reward, initialize_common_ground, select_analogical_example
 
 def main():
-    # make_grid_world_from_file("easygrid.txt", randomize=True, num_goals=1, name=None, goal_num=None, slip_prob=0.0)
-    mdp = GridWorldMDPClass.make_grid_world_from_file("easygrid.txt")
-    ql_agent = QLearningAgent(actions=mdp.get_actions()) 
-    # train policy
-    run_single_agent_on_mdp(ql_agent, mdp, episodes=1000, steps=200, verbose=False)
-    # run agent according to policy
-    traj, reward = run_mdp(ql_agent, mdp)
-    
-    # -----------------------
-    # create second trajectory
-    mdp2 = GridWorldMDPClass.make_grid_world_from_file("easygrid.txt", randomize=True, num_goals=1, name=None, goal_num=None, slip_prob=0.1)
-    ql_agent2 = QLearningAgent(actions=mdp.get_actions()) 
-    # train policy
-    run_single_agent_on_mdp(ql_agent2, mdp2, episodes=1000, steps=200, verbose=False)
-    # run agent according to policy
-    traj2, reward2 = run_mdp(ql_agent2, mdp2)
     # -------------------------
     # -----------------------
-    # create third trajectory
-    mdp3 = GridWorldMDPClass.make_grid_world_from_file("easygrid.txt", randomize=True, num_goals=1, name=None, goal_num=None, slip_prob=0.5)
-    ql_agent3 = QLearningAgent(actions=mdp.get_actions()) 
-    # train policy
-    run_single_agent_on_mdp(ql_agent3, mdp3, episodes=1000, steps=200, verbose=False)
-    # run agent according to policy
-    traj3, reward3 = run_mdp(ql_agent3, mdp3)
+    # STEP 1: Create Trajectories (via optimal policies)
+    optimal_trajectories, rewards = create_optimal_trajectories(map_name="easygrid.txt", num_agents=3, episodes=1000, steps=200, slip_prob=0.1)
+    # print(optimal_trajectories)
+    # print(rewards)
     # -------------------------
+    # -----------------------
     
-    # traj_embedding_info = [(traj, reward, mdp)]
-    traj_embedding_info = [(traj, reward, mdp), (traj2, reward2, mdp2), (traj3, reward3, mdp3)]
-    trajectories = create_embeddings(traj_embedding_info)
-    find_closest_reward(trajectories, reference_traj=trajectories[0], K=1)
+    # -------------------------
+    # -----------------------
+    # STEP 2: Create embeddings of the trajectories (using whatever technique)
+    # -------------------------
+    # -----------------------
+    embedded_trajectories = create_embeddings(optimal_trajectories, rewards, "reward-trajectory")
+    # print(embedded_trajectories)
+    # -------------------------
+    # -----------------------
+    # STEP 3: Use parallelogram method
+    # -------------------------
+    # -----------------------
+
+
     
-    # visualize policy
-    mdp.visualize_agent(ql_agent)
+    # # traj_embedding_info = [(traj, reward, mdp)]
+    # traj_embedding_info = [(traj, reward, mdp), (traj2, reward2, mdp2), (traj3, reward3, mdp3)]
+    # trajectories = create_embeddings(traj_embedding_info)
+    
+    # common_ground = initialize_common_ground(trajectories, 1)
+    # print("Common ground: ", common_ground)
+    
+    # # remove values in common ground from trajectories
+    # unknown_ground = [i for i in trajectories if i not in common_ground]
+    
+    # closest_reward = find_closest_reward(trajectories, reference_traj=trajectories[0], K=1, exclude=unknown_ground)
+    # print("Closest reward: ", closest_reward)
+    
+    # analogical_example = select_analogical_example(trajectories)
+    # print("Analogical example: ", analogical_example)
+    # # visualize policy
+    # mdp.visualize_agent(ql_agent)
+    
+
+def create_optimal_trajectories(map_name="easygrid.txt", num_agents=3, episodes=1000, steps=200, slip_prob=0.1):
     
     
+    mdps = [GridWorldMDPClass.make_grid_world_from_file(map_name, randomize=True, num_goals=1, name=None, goal_num=None, slip_prob=slip_prob) for _ in range(0, num_agents)]
+    actions = mdps[0].get_actions()
     
+    q_learning_agents = [QLearningAgent(actions=actions) for _ in range(0, num_agents)]
+
+    # train policy
+    for i in range(0, len(q_learning_agents)):
+        run_single_agent_on_mdp(q_learning_agents[i], mdps[i], episodes=episodes, steps=steps, verbose=False)
+
+    # get the trajectories and their associated rewards
+    optimal_trajectories = []
+    rewards = []
+    for i in range(0, num_agents):
+        traj, r = _run_mdp(q_learning_agents[i], mdps[i])
+        optimal_trajectories.append(traj)
+        rewards.append(r)
+    
+    # return a list of optimal trajectories
+    return optimal_trajectories, rewards
+
+
+def _run_mdp(agent, mdp):
+    trajectory = [] 
+    rewards = []
+    state = mdp.get_init_state()
+
+    while not state.is_terminal():
+        # Compute the agent's policy.
+        action = agent.act(state, None, learning=False)
+
+        # Terminal check.
+        if state.is_terminal():
+            break
+
+        # Execute in MDP.
+        _, next_state = mdp.execute_agent_action(action)
+
+        trajectory.append((state, action))
+        rewards.append(agent.get_value(state) )
+
+        # Update pointer.
+        state = next_state
+
+    # Reset the MDP, tell the agent the episode is over.
+    mdp.reset()
+    agent.end_of_episode()
+    return trajectory, rewards
+
+
 if __name__ == "__main__":
     main()
--- trajectory.py
+++ trajectory.py
@@ -2,101 +2,222 @@ from AE import AE
 import torch
 import torch.utils.data as data_utils
 import numpy as np
+# import regex_utils
+import re
 
-class Traj:
-
-    def __init__(self, traj, reward, common_ground=False):
-        self.traj = traj
-        self.reward = reward
-        self.common_ground = common_ground
-        self.embedding = self.calc_embedding()
+def _create_embedding(trajectory, reward, embedding_type):
+    get_sa_sequence(trajectory)
+    if embedding_type == "reward-trajectory": 
+        return [0, 0, 0, 0, 0, 0, 0, 0, 0]
         
-    def calc_embedding(self):
         
-        return self.traj
+def get_sa_sequence(trajectory):
+    # use regular expression to capture the pair of numbers 
+    sequence = [t[0] for t in trajectory]
+    print(sequence[0])
+    # print(re.match(str(sequence[0]), "([0-9]+, [0-9]+)"))
+    # print(str(sequence[0]))
+    match = re.search(r'^.*(\d+,.*)$', str(sequence[0]))
+    print(match.groups())
+    # print(match)
+    # print(re.search(r'.*', str(sequence[0])))
     
-def create_embeddings(traj_info_list):
-    # traj_info_list = [traj_info1, traj_info2, ...]
-    # traj_info = (traj, reward, environment)
-
-    # input = state-action sequence  
-    # output = environment??
- 
-
-    # get a list of state/actions in trajectory
-    # [[traj1],[traj2], ...]
-    input_list = []
-    for t in traj_info_list:
-        input_list.append(traj_to_int(t[0]))
+def create_embeddings(trajectories, reward, embedding_type):
+    return [_create_embedding(t, reward, embedding_type) for t in trajectories]
+    
         
-    # make all trajectories of the same length
-    input_list = _make_length_uniform(input_list)
+# class Traj:
+#     """
+#     A class representing a trajectory with associated reward and embedding.
 
-    # print("INPUT: ", input_list)
+#     Attributes:
+#     traj (list): A list of state-action pairs representing the trajectory.
+#     reward (float): The reward associated with the trajectory.
+#     common_ground (bool): A flag indicating whether the trajectory is part of common ground.
+#     embedding (torch.nn.Module): The trained encoder module of the AE model for the trajectory.
+#     """
 
-    output_list = [8 for i in input_list]
-    # print("OUTPUT: ", output_list)
+#     def __init__(self, traj, reward, embedding):
+#         """
+#         Initialize a new Traj object.
 
-    model = AE(input_dim=len(input_list))
+#         Parameters:
+#         traj (list): A list of state-action pairs representing the trajectory.
+#         reward (float): The reward associated with the trajectory.
+#         embedding (torch.nn.Module): The trained encoder module of the AE model for the trajectory.
+#         """
+#         self.traj = traj
+#         self.reward = reward
+#         self.common_ground = False
+#         self.embedding = embedding
+
     
-    # print("FEATURES: ", input_list)
-    features = torch.FloatTensor(input_list)
-    labels = torch.FloatTensor(output_list)
+# def create_embeddings(traj_info_list):
+#     """
+#     Create embeddings for a list of trajectories using an Autoencoder (AE) model.
 
-    train = data_utils.TensorDataset(features, labels)
-    train_loader = data_utils.DataLoader(train, batch_size=50, shuffle=True)
+#     This function takes a list of trajectory information tuples, processes the trajectories, adds environmental information,
+#     and trains an AE model to generate embeddings for each trajectory.
 
+#     Parameters:
+#     traj_info_list (list): A list of trajectory information tuples. Each tuple contains:
+#         - traj (list): A list of state-action pairs representing a trajectory.
+#         - reward (float): The reward associated with the trajectory.
+#         - environment (object): The environment in which the trajectory was generated.
+
+#     Returns:
+#     list: A list of trajectory objects. Each trajectory object contains:
+#         - traj (list): The original trajectory.
+#         - reward (float): The reward associated with the trajectory.
+#         - embedding (torch.nn.Module): The trained encoder module of the AE model for the trajectory.
+#     """
     
-    model.train(train_loader, 100)
+#     # get a list of state/actions in trajectory and add them to environmental information
+#     input_list = []
+#     for t in traj_info_list:
+#         input_list.append(_traj_to_int(t[0]) + _environment_to_int(t[2]))
+
+#     # make all trajectories of the same length
+#     input_list = _make_length_uniform(input_list)
+
+#     # prepare output list with a constant value for training
+#     output_list = input_list#[8 for i in input_list]
+
+#     # initialize the Autoencoder model with input dimension equal to the length of a trajectory
+#     model = AE(input_dim=len(input_list))
+
+#     # prepare features and labels for training
+#     features = torch.FloatTensor(input_list)
+#     labels = torch.FloatTensor(output_list)
+
+#     # create a TensorDataset and DataLoader for training
+#     train = data_utils.TensorDataset(features, labels)
+#     train_loader = data_utils.DataLoader(train, batch_size=50, shuffle=True)
+
+#     # train the Autoencoder model
+#     model.train(train_loader, 100)
+
+#     # create trajectory objects with the original trajectory, reward, and trained encoder module
+#     trajectories = []
+#     for traj_num in range(0, len(traj_info_list)):
+#         trajectories.append(Traj(traj_info_list[traj_num][0], traj_info_list[traj_num][1], model.encoder[traj_num]))
+
+#     return trajectories
+
+
+# def find_closest_reward(trajectories, reference_traj, K=5, exclude=[]):
+#     """
+#     Find the K trajectories with the closest rewards to a given reference trajectory.
+
+#     This function takes a list of trajectories, a reference trajectory, and an optional parameter K.
+#     It sorts the trajectories by their rewards, removes the reference trajectory from the list,
+#     calculates the absolute difference in rewards between the reference trajectory and each remaining trajectory,
+#     and selects the K trajectories with the smallest differences.
+
+#     Parameters:
+#     trajectories (list): A list of trajectory objects. Each trajectory object contains a trajectory and its reward.
+#     reference_traj (Traj): The reference trajectory for which to find the closest trajectories.
+#     K (int, optional): The number of closest trajectories to find. Default is 5.
+
+#     Returns:
+#     list: A list of K trajectory objects that have the closest rewards to the reference trajectory.
+#     """
+#     if len(exclude) > 0:
+#         # remove excluded trajectories from the list
+#         for t in exclude:
+#             if t in trajectories:
+#                 trajectories.remove(t)
+
+#     # create a list of trajectories that is sorted by reward
+#     sorted_trajectories = sorted(trajectories, key=lambda x: x.reward)
+#     if reference_traj in sorted_trajectories:
+#         sorted_trajectories.remove(reference_traj)
+
+#     # find the absolute difference in reward between reference_traj and each trajectory
+#     absolute_difference = [abs(x.reward - reference_traj.reward) for x in sorted_trajectories]
+
+#     # find K number of closest trajectories 
+#     closest_indices = sorted(range(len(absolute_difference)), key=lambda sub: absolute_difference[sub])[:K]
+#     closest_trajectories = [sorted_trajectories[i] for i in closest_indices]
+
+#     return closest_trajectories
+
+
+# def initialize_common_ground(trajectories, N):
+#     """
+#     Initialize a subset of trajectories as common ground.
+
+#     This function selects N random trajectories from the given list and marks them as common ground.
+#     The common ground is represented as a list of trajectory objects.
+
+#     Parameters:
+#     trajectories (list): A list of trajectory objects. Each trajectory object contains a trajectory and its reward.
+#     N (int): The number of trajectories to be selected as common ground.
+
+#     Returns:
+#     list: A list of trajectory objects that have been selected as common ground.
+#     """
+#     # Choose N trajectories to add to the common ground
+#     common_ground = []
+#     for _ in range(N):
+#         random_index = np.random.randint(0, len(trajectories))
+#         common_ground.append(trajectories[random_index])
+#         trajectories[random_index].common_ground = True
+
+#     return common_ground
+
+# def select_analogical_example(trajectories):
+#     """
+#     Select a random trajectory that is not part of common ground.
+
+#     This function randomly selects a trajectory from the given list of trajectory objects.
+#     It ensures that the selected trajectory is not part of common ground by checking the 'common_ground' attribute of each trajectory.
+
+#     Parameters:
+#     trajectories (list): A list of trajectory objects. Each trajectory object contains a trajectory and its reward.
+
+#     Returns:
+#     Traj: A randomly selected trajectory object that is not part of common ground.
+#     """
+#     random_index = np.random.randint(0, len(trajectories))
+#     while trajectories[random_index].common_ground:
+#         random_index = np.random.randint(0, len(trajectories))
+
+#     return trajectories[random_index]
     
-    trajectories = []
-    for traj_num in range(0, len(traj_info_list)):
-        # trajectories.append(Traj(traj_info_list[traj_num][0], traj_info_list[traj_num][1], model.encoder[traj_num].weight))
-        trajectories.append(Traj(traj_info_list[traj_num][0], traj_info_list[traj_num][1], model.encoder[traj_num]))
+# # Utility Functions
+# def _traj_to_int(traj):
+#     numerical_traj = []
+#     pair_sum = 0
+    
+#     # (state, action), (state, action)
+#     for state_action in traj:
+#         pair = str(state_action[0]) + str(state_action[1])
+#         for letter in pair:
+#             pair_sum += ord(letter)
+
+#         numerical_traj.append(pair_sum)
+#     return numerical_traj
+
+# def _environment_to_int(environment):
+#     int_environment = []
+#     str_environment = str(environment)
+#     for letter in str_environment:
+#         int_environment.append(ord(letter))
         
+#     return int_environment
 
-    return trajectories
-
-def find_closest_reward(trajectories, reference_traj, K=5):
-    # create a list of trajectories that is sorted by reward
-    sorted_trajectories = sorted(trajectories, key=lambda x: x.reward)
-    sorted_trajectories.remove(reference_traj)
-
-    # find the absolute difference in reward between reference_traj and each trajectory
-    absolute_difference = [abs(x.reward - reference_traj.reward) for x in sorted_trajectories]
-    print(absolute_difference)
-    
-    # find K number of closest trajectories 
-    closest_indices = sorted(range(len(absolute_difference)), key=lambda sub: absolute_difference[sub])[:K]
-    closest_trajectories = [sorted_trajectories[i] for i in closest_indices]
-    
-    return closest_trajectories
-
-    
-def traj_to_int(traj):
-    numerical_traj = []
-    pair_sum = 0
-    
-    # (state, action), (state, action)
-    for state_action in traj:
-        pair = str(state_action[0]) + str(state_action[1])
-        for letter in pair:
-            pair_sum += ord(letter)
-
-        numerical_traj.append(pair_sum)
-    return numerical_traj
-
-# NOTE: this definitely isn't the best way to do this
-def _make_length_uniform(input_list):
-    longest = 0
-    # find longest trajectory
-    for i in input_list:
-        if len(i) > longest:
-            longest = len(i)
+# # NOTE: this definitely isn't the best way to do this
+# def _make_length_uniform(input_list):
+#     longest = 0
+#     # find longest trajectory
+#     for i in input_list:
+#         if len(i) > longest:
+#             longest = len(i)
             
-    # make the other trajectories the same length
-    for i in input_list:
-        if len(i) < longest:
-            i.extend([0]*(longest - len(i)))
+#     # make the other trajectories the same length
+#     for i in input_list:
+#         if len(i) < longest:
+#             i.extend([0]*(longest - len(i)))
     
-    return input_list 
+#     return input_list 
