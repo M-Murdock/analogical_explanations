7e2cb63a077bff3a6119198bdc6d2fb6c8fcd933
Mavis Murdock <mavis.murdock@tufts.edu>
2025-01-28 09:29:16 -0500

    represent relationships with graph

--- vlm_similarities_graph.py
+++ vlm_similarities_graph.py
@@ -8,16 +8,15 @@ import PIL.Image
 import re
 import numpy as np
 import json
+from enum import Enum
 
 # Feeds images of robot trajectories into the VLM. Assumes that we already have the images.
     
 def main():
     genai.configure(api_key='AIzaSyC5ZYeBc-iVnqegb_X_EvIrc_Awa_6uCgk')
-    # model = genai.GenerativeModel(model_name="gemini-1.5-flash") # garbage  
+
     model = genai.GenerativeModel(model_name="gemini-2.0-flash-exp")
 
-    # IMG_DIR = "trajectory_imgs/"
-    # IMG_DIR = "icecream_traj_imgs/"
     IMG_DIR = "monocolor_icecream_traj_imgs/"
     
     # a list of paths to images
@@ -40,9 +39,7 @@ def main():
     # ----------------------------------------------------------------
     # ----------------------------------------------------------------
     print("================================")
-    # Build a database of images and their relationships to one another 
-    # e.g. [{"A:pic1", "B:pic2", "relationship:shorter"}, {"A:", "B:", "relationship...""}]
-    database = []
+    # Build a tree of trajectories and their relationships to each other 
 
     # Get all the relationship prompts
     prompt = ""
@@ -54,35 +51,59 @@ def main():
     # prompt gemini 
     img_files.insert(0, prompt)
     relationship_response = model.generate_content(img_files) 
-    print(relationship_response.text)
-    # print("relationship response: {}".format(relationship_response))
     relationship_list = relationship_response.text.split("|")
-    # convert 1d array to 2d: [1,2,3,...] --> [[1,2,3],[4,5,6]]
+    # # convert 1d array to 2d: [1,2,3,...] --> [[1,2,3],[4,5,6]]
     relationship_list = np.reshape(relationship_list, (-1, NUM_IMGS)).tolist()
 
 
+    # make nodes for every image file
+    nodes = [TrajNode(i) for i in range(0, NUM_IMGS)]
+
+    
     # create the database
     for i in range(0, NUM_IMGS):
-        database.append([])
         for j in range(0, NUM_IMGS):
-            database[-1].append({"relationship":relationship_list[i][j], "A":i, "B":j}) 
+            nodes[i].add_edge(DirectedEdge(nodes[i], nodes[j], relationship_list[i][j]))
+    nodes[0].edges[0].print_edge()
 
-
-    # save database
-    with open('database.json', 'w') as fout:
-        json.dump(database, fout)
+    # # save database
+    # with open('database.json', 'w') as fout:
+    #     json.dump(database, fout)
         
     
-    # load database 
-    with open("database.json", "r") as f:
-        data = json.load(f)
-        print(data)
+    # # load database 
+    # with open("database.json", "r") as f:
+    #     data = json.load(f)
+    #     print(data)
 
     
-    print("================================")
+    # print("================================")
+    
 
     
-    
+# Edge directed away from a (a --> b) which indicates the "a is to b" relationship
+class DirectedEdge():
+    def __init__(self, a, b, r):
+        self.a = a
+        self.b = b
+        self.r = r
+    def print_edge(self):
+        print("A: {}, B: {}, R: {}".format(self.a, self.b, self.r))
+
+class TrajNode():
+    def __init__(self, i):
+        self.i = i
+        # self.img = img
+        self.edges = []
+    def add_edge(self, e):
+        self.edges.append(e)
+    def print_node(self):
+        print("i: {}, edges: {}".format(self.i, self.edges))
+        
+class R(Enum):
+    SHORTER_THAN = 1
+    LONGER_THAN = 2
+    # ...
     
 if __name__ == "__main__":
     main()
